21c26018e26cf16a6b790f63e8557e0f354bb537
Marie (Cho) Hughes <kchohee0630@gmail.com>
2020-01-30 16:51:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,12 +2,22 @@ require_relative "../config/environment.rb"
 
 class Student
   attr_accessor
+  # attributes
+  #   has a name and a grade
+  #   has an id that defaults to `nil` on initialization
   def initialize
   end
+  # .create_table
+  #   creates the students table in the database
   def self.create_table
   end
+  # .drop_table
+  #   drops the students table from the database
   def self.drop_table
   end
+  #save
+  #   saves an instance of the Student class to the database and then sets the given students `id` attribute 
+  
   def save
   end
   def self.create
@@ -19,23 +29,13 @@ class Student
   def update
   end
 end
-
-
-  # attributes
-  #   has a name and a grade (FAILED - 1)
-  #   has an id that defaults to `nil` on initialization (FAILED - 2)
-  # .create_table
-  #   creates the students table in the database (FAILED - 3)
-  # .drop_table
-  #   drops the students table from the database (FAILED - 4)
-  # #save
-  #   saves an instance of the Student class to the database and then sets the given students `id` attribute 
+  
   #   updates a record if called on an object that is already persisted
   # .create
-  #   creates a student with two attributes, name and grade, and saves it into the students table. (FAILED - 7)
+  #   creates a student with two attributes, name and grade, and saves it into the students table.
   # .new_from_db
-  #   creates an instance with corresponding attribute values (FAILED - 8)
+  #   creates an instance with corresponding attribute values
   # .find_by_name
-  #   returns an instance of student that matches the name from the DB (FAILED - 9)
+  #   returns an instance of student that matches the name from the DB
   # #update
-  #   updates the record associated with a given instance (FAILED - 10)
+  #   updates the record associated with a given instance
