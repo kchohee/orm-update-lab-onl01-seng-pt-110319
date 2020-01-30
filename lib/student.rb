ab05a36583a61eafcbbb4556339afeda91d681f3
Marie (Cho) Hughes <kchohee0630@gmail.com>
2020-01-30 17:01:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,28 +6,46 @@ class Student
   #   has a name and a grade
   #   has an id that defaults to `nil` on initialization
   def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
   end
 
   # .create_table
   #   creates the students table in the database
   def self.create_table
+    sql = <<- SQL
+      CREATE TABLE IF NOT EXIST students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+        )
+      SQL
+      DB[:conn].execute(sql)
   end
 
   # .drop_table
   #   drops the students table from the database
   def self.drop_table
+    sql = <<- SQL
+      DROP TABLE students
+      SQL
+      DB[:conn].execute(sql)
   end
 
   #save
   #   saves an instance of the Student class to the database and then sets the given students `id` attribute
   #   updates a record if called on an object that is already persisted
   def save
-    sql = <<-SQL
-      INSERT INTO songs (name, album)
-      VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.album)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO student (name, grade)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
   
