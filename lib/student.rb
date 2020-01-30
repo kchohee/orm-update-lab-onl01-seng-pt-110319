88a75e51ceeab89bcbe608f77fefb12cffa98efd
Marie (Cho) Hughes <kchohee0630@gmail.com>
2020-01-30 16:56:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,11 +23,12 @@ class Student
   #   updates a record if called on an object that is already persisted
   def save
     sql = <<-SQL
-    INSERT INTO songs (name, grade)
-    VALUES (?, ?)
-  SQL
-  DB[:conn].execute(sql, self.name, self.grade)
-  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.album)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
   end
   
   # .create
