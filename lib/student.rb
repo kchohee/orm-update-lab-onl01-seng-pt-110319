96ac57796391ab84ca6e801ab268dfeaab0eba41
Marie (Cho) Hughes <kchohee0630@gmail.com>
2020-01-30 16:47:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,5 +6,15 @@ class Student
   end
   def self.create_table
   end
-  def 
+  def self.drop_table
+  end
+  def save
+  end
+  def self.create
+  end
+  def self.new_from_db
+  end
+  def self.find_by_name
+  end
+
 end
